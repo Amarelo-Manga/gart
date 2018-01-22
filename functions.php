b18f60ba6d0bfad787558bfe58aef85622a2954e
Elvis B. Martins <elvisbmartins@gmail.com>
2018-01-22 16:45:02 -0200

    remove version for scripts css and js

--- functions.php
+++ functions.php
@@ -557,3 +557,13 @@ function style_or_min_style( $stylesheet_uri, $stylesheet_dir_uri ) {
 }
 add_filter( 'stylesheet_uri', 'style_or_min_style', 10, 2);
 
+
+// remove wp version param from any enqueued scripts
+function vc_remove_wp_ver_css_js( $src ) {
+    if ( strpos( $src, 'ver=' ) )
+        $src = remove_query_arg( 'ver', $src );
+    return $src;
+}
+add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+
